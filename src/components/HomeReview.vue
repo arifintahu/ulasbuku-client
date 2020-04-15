5714bb8841b86151a518b851553f622d1c070c99
Miftahul Arifin <miftahul97@gmail.com>
2020-04-15 12:38:04 +0700

    fix review text style

--- src/components/HomeReview.vue
+++ src/components/HomeReview.vue
@@ -24,7 +24,7 @@
             writer  : "Miftahul Arifin",
             name    : "istanbul",
             imageSrc  : "https://1.bp.blogspot.com/-Xws1hzF69rw/XZn1WBPdaxI/AAAAAAAAAIY/x42aJCAzh3che0_iwuDZCFC-zIWqiG1RQCLcBGAsYHQ/s640/Orhan%2BPamuk%2BIstanbul.jpeg",
-            body    : "Orhan Pamuk seorang sastrawan turki dan juga peraih nobel sastra. Dalam berbagai karya novelnya, ia sangat lihai mengisahkan kota kelahirannya, Istanbul. Ia lahir setelah revolusi Turki yakni pada tahun 1952. Di masa kecilnya, ia banyak mengamati tetang kondisi kota kelahirannya yang berubah drastis paska revolusi. Orhan Pamuk memiliki keterikatan yang sangat kuat dengan Istanbul, dibuktikan dengan banyaknya literatur sejarah yang ia pelajari melalui perpustakaan pribadinya.\n Orhan Pamuk ialah seorang novelis yang memiliki keterikatan kuat pada kota kelahirannya. Novel-novelnya menceritakan dilema yang dialami oleh bangsa Turki paska revolusi. Ia terlahir dari keluarga yang sangat mapan. Kecintaannya terhadap buku dan keterikatan kuat terhadap Istanbul membuatnya banyak menceritakan tentang bangsa Turki dalam karya novel-novelnya."
+            body    : "Orhan Pamuk seorang sastrawan turki dan juga peraih nobel sastra. Dalam berbagai karya novelnya, ia sangat lihai mengisahkan kota kelahirannya, Istanbul. Ia lahir setelah revolusi Turki yakni pada tahun 1952. Di masa kecilnya, ia banyak mengamati tetang kondisi kota kelahirannya yang berubah drastis paska revolusi. Orhan Pamuk memiliki keterikatan yang sangat kuat dengan Istanbul, dibuktikan dengan banyaknya literatur sejarah yang ia pelajari melalui perpustakaan pribadinya.\n \n Orhan Pamuk ialah seorang novelis yang memiliki keterikatan kuat pada kota kelahirannya. Novel-novelnya menceritakan dilema yang dialami oleh bangsa Turki paska revolusi. Ia terlahir dari keluarga yang sangat mapan. Kecintaannya terhadap buku dan keterikatan kuat terhadap Istanbul membuatnya banyak menceritakan tentang bangsa Turki dalam karya novel-novelnya."
           }
         },
         {
@@ -35,7 +35,7 @@
             writer  : "Miftahul Arifin",
             name    : "financial-education",
             imageSrc  : "https://1.bp.blogspot.com/-zR_RjuRWhvI/XYdQWeqtX5I/AAAAAAAAB5w/jW_2W-eVBK0Il2Z728Aq2S-SAY9sIFcawCLcBGAsYHQ/s640/why%2Ba%2Bstudent.jpeg",
-            body    : "Buku karya Robert Kiyosaki ini mengupas tetang pentingnya pendidikan keuangan sebagai bekal untuk menghadapi masa depan. Pendidikan keuangan umumnya tidak diajari di sekolah-sekolah, sementara pendidikan keuangan sangat diperlukan untuk mengetahui bagaimana membuat uang bekerja pada kita, bukannya kita bekerja untuk uang.\n Kita tidak sedang mengalami krisis keuangan, tetapi kita mengalami krisis pendidikan keuangan. Semua ini bermula ketika seorang anak masuk sekolah, kemudian menghabiskan waktu tahun demi tahun namun tidak mempelajari apapun soal uang. Sistem pendidikan saat ini lebih banyak mempelajari bagaimana mencari pekerjaan daripada bagaimana membuka lapangan pekerjaan baru."
+            body    : "Buku karya Robert Kiyosaki ini mengupas tetang pentingnya pendidikan keuangan sebagai bekal untuk menghadapi masa depan. Pendidikan keuangan umumnya tidak diajari di sekolah-sekolah, sementara pendidikan keuangan sangat diperlukan untuk mengetahui bagaimana membuat uang bekerja pada kita, bukannya kita bekerja untuk uang. \n \n Kita tidak sedang mengalami krisis keuangan, tetapi kita mengalami krisis pendidikan keuangan. Semua ini bermula ketika seorang anak masuk sekolah, kemudian menghabiskan waktu tahun demi tahun namun tidak mempelajari apapun soal uang. Sistem pendidikan saat ini lebih banyak mempelajari bagaimana mencari pekerjaan daripada bagaimana membuka lapangan pekerjaan baru."
           }
         },
         {
@@ -46,7 +46,7 @@
             writer  : "Miftahul Arifin",
             name    : "the-good-father",
             imageSrc  : "https://1.bp.blogspot.com/-nz5p2jELIY4/XXZbSgNwCYI/AAAAAAAAAII/g5A3ZqFCiVQjhoWNfzs5LOF8sTQpWLMqACLcBGAs/s640/menjadi%2Bayah%2Bjpg.jpg",
-            body    : "Riset tentang pendidikan keluarga di Indonesia masih sedikit. Artinya, pendidikan keluarga belum menjadi hal yang cukup dipentingkan untuk dibahas, padahal keluarga adalah suatu bagian terpenting dalam membangun manusia juga dalam membangun suatu negara.\n Banyak kita amati dari berbagai sejarah dahulu bahwa visi negara dibangun dari visi keluarga. Misalnya dinasti utsmani, abasiyyah, dan dinasti besar lainnya dibangun berasal dari idealisme keluarga. Namun, ketika nilai-nilai keluarga tersebut dirusak, maka berujung pada social collapse dan hancurnya peradaban."
+            body    : "Riset tentang pendidikan keluarga di Indonesia masih sedikit. Artinya, pendidikan keluarga belum menjadi hal yang cukup dipentingkan untuk dibahas, padahal keluarga adalah suatu bagian terpenting dalam membangun manusia juga dalam membangun suatu negara.\n \n Banyak kita amati dari berbagai sejarah dahulu bahwa visi negara dibangun dari visi keluarga. Misalnya dinasti utsmani, abasiyyah, dan dinasti besar lainnya dibangun berasal dari idealisme keluarga. Namun, ketika nilai-nilai keluarga tersebut dirusak, maka berujung pada social collapse dan hancurnya peradaban."
           }
         },
       ]
--- src/components/ReviewPost.vue
+++ src/components/ReviewPost.vue
@@ -8,22 +8,18 @@
           	height="400"
           	class="grey darken-4"
         ></v-img>
-        <v-container class="content pa-6">
+        <v-container class="content pa-6 ">
         	<section id="title">
-        		<v-list-item class="px-0">
-			      <v-list-item-content>
-			        <v-list-item-title class="display-2">{{content.title}}</v-list-item-title>
-			        <v-list-item-subtitle class="body-1">
-			            <v-list-item-avatar size="24" class="mr-1" color="grey">
-			               <v-img :src="avatar"></v-img>
-			            </v-list-item-avatar>
-			            {{content.writer}}
-			        </v-list-item-subtitle>
-			      </v-list-item-content>
-			    </v-list-item>
+        		<div class="display-2">{{content.title}}</div>
+		        <div class="body-1 py-2">
+		            <v-avatar size="24" class="mr-1" color="grey">
+		               <v-img :src="avatar"></v-img>
+		            </v-avatar>
+		            {{content.writer}}
+		        </div>
         	</section>
         	<section id="body">
-        		<div class="body-2">
+        		<div class="body-1 text-justify body-content py-6">
         			{{content.body}}
         		</div>
         	</section>
@@ -41,3 +37,10 @@
 		})
 	}
 </script>
+
+<style scoped>
+	.body-content {
+		line-height: 1.7rem;
+		white-space: pre-line;
+	}
+</style>
